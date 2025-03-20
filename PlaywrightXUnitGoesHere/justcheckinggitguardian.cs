b3d6a2400539eb28db208947c3770aee0060f094
Phil-NHS <philip.tate@nhs.net>
2025-03-20 13:48:53 +0000

    gitguardian

--- PlaywrightXUnitGoesHere/justcheckinggitguardian.cs
+++ PlaywrightXUnitGoesHere/justcheckinggitguardian.cs
@@ -1,2 +1,3 @@
 ﻿string aws_access_key = "AKIAF6B5SAWSZ5345678";
 string secret = "hjohnk4ex5l33565d4634HNKGxhz545e89cjyjad";
+//had to remove git guardian couldnt work with repos made by raised admin and not go necessary privilage to do whats needs
