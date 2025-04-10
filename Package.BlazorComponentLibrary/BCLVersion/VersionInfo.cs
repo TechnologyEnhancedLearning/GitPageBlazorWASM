02855e24a55c6619babc990bf65aea7cf2cbe6ec
Phil-NHS <philip.tate@nhs.net>
2025-04-10 16:33:19 +0100

    fix(noo): gg

--- Package.BlazorComponentLibrary/BCLVersion/VersionInfo.cs
+++ Package.BlazorComponentLibrary/BCLVersion/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿namespace Package.BlazorComponentLibrary.BCLVersion { public static class VersionInfo { public static string Version = "3.0.9" ;
+﻿namespace Package.BlazorComponentLibrary.BCLVersion { public static class VersionInfo { public static string Version = "3.1.5" ;
 } }
