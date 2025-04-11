621daa7c3d6474d24042b1953645b6a5c8f4531b
Phil-NHS <philip.tate@nhs.net>
2025-04-11 17:10:43 +0100

    fix(go): were using release inprogram

--- TestHostPrerenderWASM/TestHostPrerenderWASM.Client/Program.cs
+++ TestHostPrerenderWASM/TestHostPrerenderWASM.Client/Program.cs
@@ -19,7 +19,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 #endif
-
+//qqqqqq this is where it will be failing
 await builder.Build().RunAsync();
 
 
