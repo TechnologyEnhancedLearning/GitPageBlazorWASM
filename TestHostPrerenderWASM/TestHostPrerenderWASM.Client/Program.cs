8f7433e88e0d3df40d893ef3a46bb39e75153da3
Phil-NHS <philip.tate@nhs.net>
2025-04-11 17:20:13 +0100

    fix(dd): ddd

--- TestHostPrerenderWASM/TestHostPrerenderWASM.Client/Program.cs
+++ TestHostPrerenderWASM/TestHostPrerenderWASM.Client/Program.cs
@@ -7,18 +7,18 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 //Be careful we have two entry points to enable the client to be both a standalone app and the client for the server app. 
 // removing the below, app.razor, and index.html would put the project back to being just for the testing host (WebAssembly.DevServer could be removed from nuget too)
-//if (builder.HostEnvironment.IsProduction()) //qqqq worked but want release mode not to be tied to this
-//{
-//    builder.RootComponents.Add<App>("#app");
-//    builder.RootComponents.Add<HeadOutlet>("head::after");
-//    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+if (builder.HostEnvironment.IsProduction()) //qqqq worked but want release mode not to be tied to this
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-//}
-#if GH_PAGE_RELEASE
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-#endif
+}
+//#if GH_PAGE_RELEASE //put something like this back in
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//#endif
 //qqqqqq this is where it will be failing
 await builder.Build().RunAsync();
 
