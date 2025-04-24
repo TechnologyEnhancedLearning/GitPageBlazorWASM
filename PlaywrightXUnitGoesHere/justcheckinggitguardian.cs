6df71712aae2ec350ff0ff71a1f81b3717577cf9
Phil-NHS <philip.tate@nhs.net>
2025-04-24 10:55:48 +0100

    chore(check gitguard prehook): this should be blocked

--- PlaywrightXUnitGoesHere/justcheckinggitguardian.cs
+++ PlaywrightXUnitGoesHere/justcheckinggitguardian.cs
@@ -1,3 +1,5 @@
 ﻿string aws_access_key = "AKIAF6TESTDONTPANIC5678";
 string secret = "hjohnkTESTDONTPANICjyjad";
+string aws_access_key1 = "AKIAF6TESTDONTPANIC5678";
+string secret1 = "hjohnkTESTDONTPANICjyjad";
 //had to remove git guardian couldnt work with repos made by raised admin and not go necessary privilage to do whats needs
