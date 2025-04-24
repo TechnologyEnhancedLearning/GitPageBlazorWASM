d5de9a0c72af16742b512e861f2843ab0b5110bd
Phil-NHS <philip.tate@nhs.net>
2025-04-24 13:09:46 +0100

    chore(sec): block me

--- PlaywrightXUnitGoesHere/justcheckinggitguardian.cs
+++ PlaywrightXUnitGoesHere/justcheckinggitguardian.cs
@@ -2,4 +2,6 @@
 string secret = "hjohnkTESTDONTPANICjyjad";
 string aws_access_key1 = "AKIAF6TESTDONThPANIC5678";
 string secret1 = "hjohnkTESTDONTPANICjyjad";
+string aws_access_key1 = "AKIAF6TESTDONThPANIC5678";
+string secret1 = "hjohnkTESTDONTPANICjyjad";
 //had to remove git guardian couldnt work with repos made by raised admin and not go necessary privilage to do whats needs
