5cf9f5b3239dc07c8e56ade7eccefd71b59aaf73
Phil-NHS <philip.tate@nhs.net>
2025-04-24 14:40:25 +0100

    lint commits

--- PlaywrightXUnitGoesHere/justcheckinggitgy.cs
+++ PlaywrightXUnitGoesHere/justcheckinggitgy.cs
@@ -2,4 +2,6 @@
 string secretgg = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 string aws_access_key1 = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 string secret1 = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+string AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
 //had to remove git guardian couldnt work with repos made by raised admin and not go necessary privilage to do whats needs
---
+++ PlaywrightXUnitGoesHere/ns.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightBUnitGoesHere
+{
+    internal class ns
+    {
+        string aws_access_key = "AKIAF6B5SAWSZ5345678";
+        string secret = "hjohnk4ex5l33565d4634HNKGxhz545e89cjyjad";
+    }
+}
